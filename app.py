b9a2621ff0eb21ae0f03feb2fd22040cb8ad8328
AbiollaghJames <118794520+AbiollaghJames@users.noreply.github.com>
2023-08-01 11:18:43 +0000

    Added API route and more templates

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Scientist',
+    'location': 'Nairobi, Kenya',
+    'salary': 'Kes. 100, 000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Analyst',
+    'location': 'Kisumu, Kenya',
+    'salary': 'Kes. 70, 000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'Kes. 50, 000'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Mombasa, Kenya',
+    'salary': 'Kes. 80, 000'
+  },
+  {
+    'id': 5,
+    'title': 'DevOps Engineer',
+    'location': 'Remote',
+    #'salary': 'Kes. 100, 000'
+  }
+]
 
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return render_template('home.html', jobs=JOBS, company_name='Abiollagh')
+
+
+@app.route("/api/jobs")
+def job_list():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
